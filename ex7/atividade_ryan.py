7cc5a78593dff6521990462d5dfa08856e612b73
wate <ryan.o2712@gmail.com>
2026-09-01 10:57:48 -0300

    final ex7

--- ex7/atividade_ryan.py
+++ ex7/atividade_ryan.py
@@ -7,42 +7,63 @@ class SaldoInsuficienteException(Exception):
 
 
 class Estoque:
-
     def __init__(self, quantidade_inicial):
-        # TODO
-        pass
+        if quantidade_inicial < 0:
+            raise ValueError()
+        self.quantidade = quantidade_inicial
 
     def reservar(self, quantidade):
-        # TODO
-        pass
+        if quantidade <= 0:
+            raise ValueError()
+        elif quantidade > self.quantidade:
+            raise EstoqueInsuficienteException()
+        
+        self.quantidade -= quantidade
+        return True
 
     def devolver(self, quantidade):
-        # TODO
-        pass
+        if quantidade <= 0: 
+            raise ValueError()
+        self.quantidade += quantidade
 
     def get_quantidade(self):
-        # TODO
-        pass
+        return self.quantidade
 
 
 class Carteira:
-
     def __init__(self, saldo_inicial):
-        # TODO
-        pass
+        if saldo_inicial < 0:
+            raise ValueError()
+        self.saldo_disponivel = saldo_inicial
 
     def cobrar(self, valor):
-        # TODO
-        pass
+        if valor <= 0:
+            raise ValueError()
+        elif valor > self.saldo_disponivel:
+            raise SaldoInsuficienteException()
+
+        self.saldo_disponivel -= valor
 
     def get_saldo(self):
-        # TODO
-        pass
+        return self.saldo_disponivel
 
 
 class ProcessadorPedido:
-
     @staticmethod
     def processar(estoque, carteira, quantidade, preco_unitario):
-        # TODO
-        pass
+        if quantidade <= 0 or preco_unitario <= 0:
+            raise ValueError()
+
+        fez_reserva = False 
+
+        try:
+            fez_reserva = estoque.reservar(quantidade)
+            carteira.cobrar(quantidade * preco_unitario)
+        except EstoqueInsuficienteException:
+            return "Estoque insuficiente"
+        except SaldoInsuficienteException:
+            if fez_reserva:
+                estoque.devolver(quantidade)
+            return "Saldo insuficiente"
+        else:
+            return "Pedido confirmado"
